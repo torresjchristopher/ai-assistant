99eefd109cd56cf7d0e2056c960249e81e2e44f5
Chris T. <serrot.rehpotsirhc@gmail.com>
2025-11-07 15:31:22 -0500

    Test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
+import tailwindcssPreset from "@tailwindcss/preset";
+
 export default {
+  presets: [tailwindcssPreset()],
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: { extend: {} },
   plugins: [],
